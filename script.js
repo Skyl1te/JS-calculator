65925d5881c303b8a31e39c0717ef2a7f6884738
_skylite <chuchu.andreimax@gmail.com>
2022-11-05 21:05:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+var currentValue = "0"; 
+var savedValue = "0"; 
+var signMinus = false; 
+var currentOperation = null; 
+var resetOnNumberPressed = false; 
+ 
+var highlightId = null; 
+ 
+function display() { 
+    document.getElementById("display").innerHTML = signMinus ? "-" + currentValue : currentValue;
+}
+ 
+function numPressed(num) { 
+    if (resetOnNumberPressed) { 
+        reset();
+        resetOnNumberPressed = false;
+    }
+ 
+    if (currentValue == "0")
+        currentValue = String(num);
+    else
+        currentValue += num;
+    display();
+}
+ 
+function reset() { 
+    currentValue = "0";
+    savedValue = "0";
+    signMinus = false;
+    resetHighlight();
+    display();
+}
+ 
+function signChange() { 
+    if (currentValue == "0") 
+        return;
+ 
+    signMinus = !signMinus; 
+    display();
+}
+ 
+function operation(op) { 
+    if (currentOperation != null) 
+        calculate(); 
+    resetOnNumberPressed = false;
+ 
+    
+    savedValue = signMinus ? "-" + currentValue : currentValue;
+    currentValue = 0;
+    signMinus = false;
+    currentOperation = op;
+    display();
+}
+ 
+function calculate() { 
+    if (currentOperation == null) 
+        return;
+ 
+    currentValue = signMinus ? "-" + currentValue : currentValue;
+    currentValue = String(eval(savedValue + currentOperation + currentValue)); 
+ 
+    currentOperation = null;
+    savedValue = 0;
+    signMinus = false;
+    resetOnNumberPressed = true;
+ 
+    if (currentValue[0] == '-') { 
+        currentValue = currentValue.substring(1);
+        signMinus = true; 
+    }
+ 
+    resetHighlight(); 
+    display();
+}
+ 
+function backspace() {
+    if (resetOnNumberPressed) { 
+        reset();
+        return;
+    }
+ 
+    if (currentValue.length === 1) { 
+        currentValue = "0"; 
+        signMinus = false;
+    } else
+        currentValue = currentValue.substring(0, currentValue.length - 1); 
+    display();
+}
+ 
+function highlight(id) { 
+    resetHighlight();
+ 
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    highlightId = id; 
+}
+ 
+function resetHighlight() { 
+    if (highlightId != null)
+        document.getElementById(highlightId).style.backgroundColor = "";
+}
